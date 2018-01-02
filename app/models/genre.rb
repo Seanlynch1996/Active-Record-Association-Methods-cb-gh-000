8cf15e1b0e6fb81460d86dd086b8619e9e105fd6
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-02 19:48:45 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    Artist.select(:name)
+    Artist.each do |t|
+      t.name
+    end
   end
 end
